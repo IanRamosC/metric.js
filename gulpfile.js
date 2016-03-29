83c1fe160ea8fa21bacdf9466d66003718aed7c5
Ian Ramos <ianramosc@gmail.com>
2016-03-28 21:14:18 -0300

    adding gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp')
+  , jshint = require('gulp-jshint')
+  , stylish = require('jshint-stylish')
+  , uglify = require('gulp-uglify')
+  , jsPath = './src/priest.js';
+
+gulp.task('jshint', function() {
+  return gulp.src(jsPath)
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('minify', function() {
+  return gulp.src(jsPath)
+    .pipe(gulp.dest('./dist/priest.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/priest.min.js'))
+});
+
+gulp.task('default', ['watch']);
